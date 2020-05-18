8d5e16ed7fa78f9d8a005a9bd92fba43a5334d91
tati2327 <tatumora1302@gmail.com>
2020-05-17 21:49:19 -0600

    Vistas de administración listas
    Faltan detallitos, playadas mias jaja

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,27 @@ import { Routes, RouterModule } from '@angular/router';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import { AdminViewComponent } from './components/admin-view/admin-view.component';
+import { BodegViewComponent } from './components/bodeg-view/bodeg-view.component';
+import { RolesComponent } from './components/admin-view/roles/roles.component';
+import { SucursalesComponent } from './components/admin-view/sucursales/sucursales.component';
+import { TrabajadoresComponent } from './components/admin-view/trabajadores/trabajadores.component';
+import { VendedoresComponent } from './components/admin-view/vendedores/vendedores.component';
+import { ProductosComponent } from './components/admin-view/productos/productos.component';
+import { RutasComponent } from './components/admin-view/rutas/rutas.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'shop', component: ShoppingCartComponent}
+  { path: 'shoppingCart', component: ShoppingCartComponent},
+  { path: 'adminView', component: AdminViewComponent},
+  { path: 'roles', component: RolesComponent},
+  { path: 'sucursales', component: SucursalesComponent},
+  { path: 'trabajadores', component: TrabajadoresComponent},
+  { path: 'vendedores', component: VendedoresComponent},
+  { path: 'productos', component: ProductosComponent},
+  { path: 'rutas', component: RutasComponent},
+  { path: 'bodegView', component: BodegViewComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AdminViewComponent } from './components/admin-view/admin-view.component';
 import { BodegViewComponent } from './components/bodeg-view/bodeg-view.component';
+import { RolesComponent } from './components/admin-view/roles/roles.component';
+import { SucursalesComponent } from './components/admin-view/sucursales/sucursales.component';
+import { TrabajadoresComponent } from './components/admin-view/trabajadores/trabajadores.component';
+import { VendedoresComponent } from './components/admin-view/vendedores/vendedores.component';
+import { ProductosComponent } from './components/admin-view/productos/productos.component';
+import { RutasComponent } from './components/admin-view/rutas/rutas.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +38,13 @@ import { BodegViewComponent } from './components/bodeg-view/bodeg-view.component
     LoginComponent,
     RegisterComponent,
     AdminViewComponent,
-    BodegViewComponent
+    BodegViewComponent,
+    RolesComponent,
+    SucursalesComponent,
+    TrabajadoresComponent,
+    VendedoresComponent,
+    ProductosComponent,
+    RutasComponent
   ],
   imports: [
     BrowserModule,
