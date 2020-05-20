f1b9884fcb896de1192aaa6a313890e5783bcd7b
Unknown <unknown@example.com>
2020-05-19 22:06:42 -0600

    Datepicker funcional

--- src/app/components/reports/sales-view/sales-view.component.ts
+++ src/app/components/reports/sales-view/sales-view.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sales-view',
   templateUrl: './sales-view.component.html',
-  styleUrls: ['./sales-view.component.css']
+  styleUrls: ['./sales-view.component.css'],
+  
 })
 export class SalesViewComponent implements OnInit {
 
